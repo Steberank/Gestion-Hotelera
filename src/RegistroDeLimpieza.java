946408364289ee583686f082fee6db7e755e6626
Steberank <danscalzo04@gmail.com>
2025-10-25 23:12:22 -0300

    Nueva clase, RegistroDeLimpieza: -Personal mucama -LocalDate fecha -Habitacion habitacion
    //Esta clase llevara todas las limpiezas llevadas a cabo por las mucamas

---
+++ src/RegistroDeLimpieza.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+
+public class RegistroDeLimpieza {
+    private Personal mucama;
+    private LocalDate fecha;
+    private Habitacion habitacion;
+
+    public RegistroDeLimpieza(Personal mucama, LocalDate fecha, Habitacion habitacion) {
+        this.mucama = mucama;
+        this.fecha = fecha;
+        this.habitacion = habitacion;
+    }
+
+    public Personal getMucama() {
+        return mucama;
+    }
+
+    public void setMucama(Personal mucama) {
+        this.mucama = mucama;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+}
